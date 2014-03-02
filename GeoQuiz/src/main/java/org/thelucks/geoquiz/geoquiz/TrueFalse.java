96366abe29ae95d6b697c3fa318ef5e44c1b8a6e
Andrew <andrew@thelucks.org>
2014-03-02 18:20:36 -0500

    Initial commit

---
+++ GeoQuiz/src/main/java/org/thelucks/geoquiz/geoquiz/TrueFalse.java
@@ -0,0 +1,34 @@
+package org.thelucks.geoquiz.geoquiz;
+
+/**
+ * Created by Andrew on 2/25/14.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion)
+    {
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+
+    public boolean isTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setTrueQuestion(boolean trueQuestion) {
+        mTrueQuestion = trueQuestion;
+    }
+
+    public int getQuestion() {
+        return mQuestion;
+    }
+
+    public void setQuestion(int question) {
+        mQuestion = question;
+    }
+
+
+}
