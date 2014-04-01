c2111201b768522739f0b30cd7db5b7905ced11e
Andrew <andrew@thelucks.org>
2014-04-01 19:27:47 -0400

    Initial version

---
+++ GeoQuiz/src/main/java/org/thelucks/geoquiz/geoquiz/CheatActivity.java
@@ -0,0 +1,17 @@
+package org.thelucks.geoquiz.geoquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Andrew on 4/1/2014.
+ */
+public class CheatActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstance)
+    {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_cheat);
+    }
+}
